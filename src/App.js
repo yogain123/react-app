2629f5ad0e672b58b03fd6e9f9c91c9771142624
yogendra <yogendrasaxena56@gmail.com>
2022-03-26 10:04:20 +0530

    folder explorer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import "./App.css";
-function App() {
-  return <div>Hello</div>;
+import { folderData } from "./data";
+import { useState } from "react";
+
+function App({ data }) {
+  if (!data) {
+    data = folderData;
+  }
+  const [expand, setExpand] = useState(false);
+
+  if (data.isFolder) {
+    return (
+      <>
+        {
+          <div>
+            <span onClick={() => setExpand(!expand)}>{data.name}</span>
+            <br />
+            <div style={{ display: expand ? "block" : "none", marginLeft: 30 }}>
+              {data.child.map((item) => (
+                <App key={item.name} data={item} />
+              ))}
+            </div>
+          </div>
+        }
+      </>
+    );
+  } else {
+    return (
+      <span>
+        {data.name} <br />
+      </span>
+    );
+  }
 }
 
 export default App;
