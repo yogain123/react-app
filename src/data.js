e66ed2f580e2759414089e481bcebaf3bea390bb
yogendra <yogendrasaxena56@gmail.com>
2022-03-26 10:02:58 +0530

    initial commit react app

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const folderData = {
+  isFolder: true,
+  name: "root",
+  child: [
+    {
+      isFolder: true,
+      name: "react",
+      child: [
+        {
+          name: "react.js",
+        },
+        {
+          name: "next.js",
+        },
+        {
+          isFolder: true,
+          name: "redux",
+          child: [
+            {
+              name: "store.js",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "angular.js",
+    },
+  ],
+};
