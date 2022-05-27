25c90130927b101912f39de7fbf50bc31c794e57
yogendra <yogendrasaxena56@gmail.com>
2022-05-27 15:54:33 +0530

    add programmtive navigation

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
   return (
     <>
-      <Link to="/about">Go to About Section</Link>
+      <Link to="/about">Go to About Section using Link</Link> <br />
+      <button type="button" onClick={() => navigate("/about")}>
+        Go to about section using button
+      </button>
       <div>This is a Home Page</div>
     </>
   );
